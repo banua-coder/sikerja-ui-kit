b9cd4b246d157c7255476a7a836bd1ad40fc1bfa
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-07-22 03:29:04 +0800

    chore(styles): add colors

---
+++ lib/src/core/styles/sk_colors.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class SKColors {
+  const SKColors._();
+
+  static const Color primary = Color(0xFF303F9F);
+  static const Color primarySurface = Color(0xFFF5F5FA);
+  static const Color primaryFocus = Color(0xFFD6D9EC);
+  static const Color primaryBorder = Color(0xFFBABFDF);
+  static const Color primaryHover = Color(0xFF283584);
+  static const Color primaryPressed = Color(0xFF182050);
+
+  static const MaterialColor neutral = MaterialColor(
+    0xFFAEAEAE,
+    {
+      10: Color(0xFFD9D9D9),
+      20: Color(0xFFDCDCDC),
+      30: Color(0xFFD5D5D5),
+      40: Color(0xFFC9C9C9),
+      50: Color(0xFFAEAEAE),
+      60: Color(0xFF8E8E8E),
+      70: Color(0xFF696969),
+      80: Color(0xFF535353),
+      90: Color(0xFF393939),
+      100: Color(0xFF090909),
+    },
+  );
+
+  static const Color secondary = Color(0xFFFFC107);
+  static const Color secondarySurface = Color(0xFFFFFCF3);
+  static const Color secondaryFocus = Color(0xFFFFF3CD);
+  static const Color secondaryBorder = Color(0xFFFFC107);
+  static const Color secondaryHover = Color(0xFFD5A106);
+  static const Color secondaryPressed = Color(0xFF806003);
+  static const Color errorSurface = Color(0xFFFDF4F4);
+  static const Color errorBorder = Color(0xFFF3B5B8);
+  static const Color error = Color(0xFFDA1E28);
+  static const Color errorHover = Color(0xFFBF161F);
+  static const Color errorPressed = Color(0xFF841017);
+  static const Color successSurface = Color(0xFFF4F9F5);
+  static const Color successBorder = Color(0xFFB3D5BD);
+  static const Color success = Color(0xFF198038);
+  static const Color successHover = Color(0xFF158035);
+  static const Color successPressed = Color(0xFF105726);
+  static const Color warningSurface = Color(0xFFFEFCF4);
+  static const Color warningBorder = Color(0xFFFAEBB4);
+  static const Color warning = Color(0xFFF1C21B);
+  static const Color warningHover = Color(0xFFDAAD0F);
+  static const Color warningPressed = Color(0xFF927410);
+  static const Color infoSurface = Color(0xFFF2F6FD);
+  static const Color infoBorder = Color(0xFFABC1EF);
+  static const Color info = Color(0xFF0043CE);
+  static const Color infoHover = Color(0xFF0840B4);
+  static const Color infoPressed = Color(0xFF012A81);
+}

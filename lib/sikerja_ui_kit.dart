5cde1e62eebfb78054eff4766e4b8d42a60dfd5e
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-08-06 18:24:12 +0800

    chore(libarary): export theme

--- lib/sikerja_ui_kit.dart
+++ lib/sikerja_ui_kit.dart
@@ -1,4 +1,15 @@
 library sikerja_ui_kit;
 
-// 3rd Party Packages
+// 3rd party packages
 export 'package:flutter_screenutil/flutter_screenutil.dart';
+export 'package:nil/nil.dart';
+
+// components
+
+// styles
+export 'src/core/styles/sk_colors.dart';
+export 'src/core/styles/sk_text_style.dart';
+
+// themes
+export 'src/core/theme/sk_color_theme.dart';
+export 'src/core/theme/sk_theme.dart';

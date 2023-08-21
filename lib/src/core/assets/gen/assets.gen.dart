6b178979df091fc2a23ee06737af8db353de3e4e
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-08-21 11:02:48 +0800

    chore(assets): generate assets using fluttergen

---
+++ lib/src/core/assets/gen/assets.gen.dart
@@ -0,0 +1,402 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+import 'package:lottie/lottie.dart';
+
+class $AssetsAnimationsGen {
+  const $AssetsAnimationsGen();
+
+  /// File path: assets/animations/success.json
+  LottieGenImage get success =>
+      const LottieGenImage('assets/animations/success.json');
+
+  /// File path: assets/animations/warning.json
+  LottieGenImage get warning =>
+      const LottieGenImage('assets/animations/warning.json');
+
+  /// List of all assets
+  List<LottieGenImage> get values => [success, warning];
+}
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/advertising.png
+  AssetGenImage get advertising =>
+      const AssetGenImage('assets/icons/advertising.png');
+
+  /// File path: assets/icons/book.png
+  AssetGenImage get book => const AssetGenImage('assets/icons/book.png');
+
+  /// File path: assets/icons/bullets.png
+  AssetGenImage get bullets => const AssetGenImage('assets/icons/bullets.png');
+
+  /// File path: assets/icons/certificate.png
+  AssetGenImage get certificate =>
+      const AssetGenImage('assets/icons/certificate.png');
+
+  /// File path: assets/icons/collection.png
+  AssetGenImage get collection =>
+      const AssetGenImage('assets/icons/collection.png');
+
+  /// File path: assets/icons/event.png
+  AssetGenImage get event => const AssetGenImage('assets/icons/event.png');
+
+  /// File path: assets/icons/fashion.png
+  AssetGenImage get fashion => const AssetGenImage('assets/icons/fashion.png');
+
+  /// File path: assets/icons/filter.png
+  AssetGenImage get filter => const AssetGenImage('assets/icons/filter.png');
+
+  /// File path: assets/icons/guide.png
+  AssetGenImage get guide => const AssetGenImage('assets/icons/guide.png');
+
+  /// File path: assets/icons/headphones.png
+  AssetGenImage get headphones =>
+      const AssetGenImage('assets/icons/headphones.png');
+
+  /// File path: assets/icons/investment.png
+  AssetGenImage get investment =>
+      const AssetGenImage('assets/icons/investment.png');
+
+  /// File path: assets/icons/logistic.png
+  AssetGenImage get logistic =>
+      const AssetGenImage('assets/icons/logistic.png');
+
+  /// File path: assets/icons/market.png
+  AssetGenImage get market => const AssetGenImage('assets/icons/market.png');
+
+  /// File path: assets/icons/pharmacy.png
+  AssetGenImage get pharmacy =>
+      const AssetGenImage('assets/icons/pharmacy.png');
+
+  /// File path: assets/icons/photo-camera.png
+  AssetGenImage get photoCamera =>
+      const AssetGenImage('assets/icons/photo-camera.png');
+
+  /// File path: assets/icons/search.png
+  AssetGenImage get search => const AssetGenImage('assets/icons/search.png');
+
+  /// File path: assets/icons/tool-box.png
+  AssetGenImage get toolBox => const AssetGenImage('assets/icons/tool-box.png');
+
+  /// File path: assets/icons/work-history-filled.png
+  AssetGenImage get workHistoryFilled =>
+      const AssetGenImage('assets/icons/work-history-filled.png');
+
+  /// File path: assets/icons/work-history.png
+  AssetGenImage get workHistory =>
+      const AssetGenImage('assets/icons/work-history.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        advertising,
+        book,
+        bullets,
+        certificate,
+        collection,
+        event,
+        fashion,
+        filter,
+        guide,
+        headphones,
+        investment,
+        logistic,
+        market,
+        pharmacy,
+        photoCamera,
+        search,
+        toolBox,
+        workHistoryFilled,
+        workHistory
+      ];
+}
+
+class $AssetsIllustrationGen {
+  const $AssetsIllustrationGen();
+
+  /// File path: assets/illustration/appliance_send.png
+  AssetGenImage get applianceSend =>
+      const AssetGenImage('assets/illustration/appliance_send.png');
+
+  /// File path: assets/illustration/apply_confirmation.png
+  AssetGenImage get applyConfirmation =>
+      const AssetGenImage('assets/illustration/apply_confirmation.png');
+
+  /// File path: assets/illustration/business.png
+  AssetGenImage get business =>
+      const AssetGenImage('assets/illustration/business.png');
+
+  /// File path: assets/illustration/coming_soon.png
+  AssetGenImage get comingSoon =>
+      const AssetGenImage('assets/illustration/coming_soon.png');
+
+  /// File path: assets/illustration/complete_profile.png
+  AssetGenImage get completeProfile =>
+      const AssetGenImage('assets/illustration/complete_profile.png');
+
+  /// File path: assets/illustration/freelancer.png
+  AssetGenImage get freelancer =>
+      const AssetGenImage('assets/illustration/freelancer.png');
+
+  /// File path: assets/illustration/information.png
+  AssetGenImage get information =>
+      const AssetGenImage('assets/illustration/information.png');
+
+  /// File path: assets/illustration/job_fair.png
+  AssetGenImage get jobFair =>
+      const AssetGenImage('assets/illustration/job_fair.png');
+
+  /// File path: assets/illustration/login.png
+  AssetGenImage get login =>
+      const AssetGenImage('assets/illustration/login.png');
+
+  /// File path: assets/illustration/not_found.png
+  AssetGenImage get notFound =>
+      const AssetGenImage('assets/illustration/not_found.png');
+
+  /// File path: assets/illustration/password_change.png
+  AssetGenImage get passwordChange =>
+      const AssetGenImage('assets/illustration/password_change.png');
+
+  /// File path: assets/illustration/welcome.png
+  AssetGenImage get welcome =>
+      const AssetGenImage('assets/illustration/welcome.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        applianceSend,
+        applyConfirmation,
+        business,
+        comingSoon,
+        completeProfile,
+        freelancer,
+        information,
+        jobFair,
+        login,
+        notFound,
+        passwordChange,
+        welcome
+      ];
+}
+
+class $AssetsLogoGen {
+  const $AssetsLogoGen();
+
+  /// File path: assets/logo/sikerja.png
+  AssetGenImage get sikerja => const AssetGenImage('assets/logo/sikerja.png');
+
+  /// File path: assets/logo/sikerja_h_blue.png
+  AssetGenImage get sikerjaHBlue =>
+      const AssetGenImage('assets/logo/sikerja_h_blue.png');
+
+  /// File path: assets/logo/sikerja_h_main.png
+  AssetGenImage get sikerjaHMain =>
+      const AssetGenImage('assets/logo/sikerja_h_main.png');
+
+  /// File path: assets/logo/sikerja_h_secondary.png
+  AssetGenImage get sikerjaHSecondary =>
+      const AssetGenImage('assets/logo/sikerja_h_secondary.png');
+
+  /// File path: assets/logo/sikerja_h_white.png
+  AssetGenImage get sikerjaHWhite =>
+      const AssetGenImage('assets/logo/sikerja_h_white.png');
+
+  /// File path: assets/logo/sikerja_icon_blue.png
+  AssetGenImage get sikerjaIconBlue =>
+      const AssetGenImage('assets/logo/sikerja_icon_blue.png');
+
+  /// File path: assets/logo/sikerja_icon_main.png
+  AssetGenImage get sikerjaIconMain =>
+      const AssetGenImage('assets/logo/sikerja_icon_main.png');
+
+  /// File path: assets/logo/sikerja_icon_secondary.png
+  AssetGenImage get sikerjaIconSecondary =>
+      const AssetGenImage('assets/logo/sikerja_icon_secondary.png');
+
+  /// File path: assets/logo/sikerja_icon_white.png
+  AssetGenImage get sikerjaIconWhite =>
+      const AssetGenImage('assets/logo/sikerja_icon_white.png');
+
+  /// File path: assets/logo/sikerja_v_blue.png
+  AssetGenImage get sikerjaVBlue =>
+      const AssetGenImage('assets/logo/sikerja_v_blue.png');
+
+  /// File path: assets/logo/sikerja_v_main.png
+  AssetGenImage get sikerjaVMain =>
+      const AssetGenImage('assets/logo/sikerja_v_main.png');
+
+  /// File path: assets/logo/sikerja_v_secondary.png
+  AssetGenImage get sikerjaVSecondary =>
+      const AssetGenImage('assets/logo/sikerja_v_secondary.png');
+
+  /// File path: assets/logo/sikerja_v_white.png
+  AssetGenImage get sikerjaVWhite =>
+      const AssetGenImage('assets/logo/sikerja_v_white.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        sikerja,
+        sikerjaHBlue,
+        sikerjaHMain,
+        sikerjaHSecondary,
+        sikerjaHWhite,
+        sikerjaIconBlue,
+        sikerjaIconMain,
+        sikerjaIconSecondary,
+        sikerjaIconWhite,
+        sikerjaVBlue,
+        sikerjaVMain,
+        sikerjaVSecondary,
+        sikerjaVWhite
+      ];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsAnimationsGen animations = $AssetsAnimationsGen();
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsIllustrationGen illustration = $AssetsIllustrationGen();
+  static const $AssetsLogoGen logo = $AssetsLogoGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName);
+
+  final String _assetName;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class LottieGenImage {
+  const LottieGenImage(this._assetName);
+
+  final String _assetName;
+
+  LottieBuilder lottie({
+    Animation<double>? controller,
+    bool? animate,
+    FrameRate? frameRate,
+    bool? repeat,
+    bool? reverse,
+    LottieDelegates? delegates,
+    LottieOptions? options,
+    void Function(LottieComposition)? onLoaded,
+    LottieImageProviderFactory? imageProviderFactory,
+    Key? key,
+    AssetBundle? bundle,
+    Widget Function(BuildContext, Widget, LottieComposition?)? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    double? width,
+    double? height,
+    BoxFit? fit,
+    AlignmentGeometry? alignment,
+    String? package,
+    bool? addRepaintBoundary,
+    FilterQuality? filterQuality,
+    void Function(String)? onWarning,
+  }) {
+    return Lottie.asset(
+      _assetName,
+      controller: controller,
+      animate: animate,
+      frameRate: frameRate,
+      repeat: repeat,
+      reverse: reverse,
+      delegates: delegates,
+      options: options,
+      onLoaded: onLoaded,
+      imageProviderFactory: imageProviderFactory,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      package: package,
+      addRepaintBoundary: addRepaintBoundary,
+      filterQuality: filterQuality,
+      onWarning: onWarning,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
